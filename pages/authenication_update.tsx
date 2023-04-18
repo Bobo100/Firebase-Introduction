3d3835f494ebbf04212f239db03bf7e4f50953ab
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-04-18 17:47:39 +0800

    Update authenication_update.tsx

--- pages/authenication_update.tsx
+++ pages/authenication_update.tsx
@@ -12,8 +12,13 @@ function AuthenicationUpdatePage() {
                 </h1>
                 <p className="my-4">上一回我們學了如何使用Firebase的API來登入，現在我們要實作登入後的內容，像是讓你的使用者可以修改他的密碼，這些都是我們要學習的內容。</p>
                 <h2 className="text-xl my-4 font-bold border p-2">修改Display Name</h2>
+                <p className="my-4"></p>
+
                 <h2 className="text-xl my-4 font-bold border p-2">修改Email</h2>
+
                 <h2 className="text-xl my-4 font-bold border p-2">修改Password</h2>
+
+
 
                 <a href="https://homework-07-sso-login.vercel.app/" target="_blank" rel="noopener" className="text-black bg-title border-white font-semibold">實作範例</a>
 
